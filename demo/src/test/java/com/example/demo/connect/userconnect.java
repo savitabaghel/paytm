61e5940fa22f549ee933d052419a0b1d480fbb28
savitabaghel <69578723+savitabaghel@users.noreply.github.com>
2022-01-13 08:35:18 +0530

    Delete demo directory

--- demo/src/test/java/com/example/demo/connect/userconnect.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo.connect;
-
-
-import com.example.demo.exception.ResourceNotFundException;
-import com.example.demo.model.user;
-import com.example.demo.repository.userconnect;
-
-public class userconnect {
-}
